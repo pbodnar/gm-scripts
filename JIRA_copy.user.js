fbb341719cc34e7f1fa9f08c8f693c7ee19f69ce
Petr Bodnar <p.bodnar@centrum.cz>
2017-06-19 13:21:43 +0200

    copy-tag-like-item extension - initial implementation

--- JIRA_copy.user.js
+++ JIRA_copy.user.js
@@ -3,9 +3,10 @@
 // @namespace   pbo
 // @description JIRA - copy info to clipboard
 // @include     http://your.jira.example.com/browse/*
-// @version     1.0.0
+// @version     1.1.0
 // @grant       GM_registerMenuCommand
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js
 // @require     https://raw.githubusercontent.com/jeresig/jquery.hotkeys/0.2.0/jquery.hotkeys.js
 // ==/UserScript==
 
@@ -16,10 +17,10 @@ trace('starting...');
 function showHelp() {
   alert(`Copy Anything for JIRA - Help
   
-    Press 'D' to display the follow-up options to copy various parts of the page into the clipboard.
+    Press 'D' to display the follow-up shortcuts / options to copy various parts of the page into the clipboard.
     
     @author pbodnar
-    @version 1.0.0    
+    @version 1.1.0    
   `);
 }
 
@@ -99,7 +100,11 @@ function getCommandsDiv() {
    <li>Shift+M ... commit <u>m</u>essage template</li>
    <hr />
    <li>Shift+E ... <u>e</u>scape - hide this popup</li>
-  </ul>`;
+  </ul>
+  <p>
+  NEW since v1.1: Mouse over a tag-like item to copy its text to clipboard via a dynamic 'copy' button.
+  </p>
+  `;
   
   insertBodyElement(div);
   
@@ -230,6 +235,8 @@ function copyHtmlToClipboard(html) {
   focused.focus();
 }
 
+setInterval(extendTagLikeItems, 1000);
+
 trace('started');
 
 // ==================== Generic functions ====================
@@ -281,4 +288,115 @@ jQuery.fn.center = function () {
   this.css('left', Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
                                               $(window).scrollLeft()) + 'px');
   return this;
+}
+
+// ==================== copy-tag-like-item extension ====================
+
+/*
+ * Structure of the target element:
+ * 
+ * a) readonly mode (example: "Labels" field):
+ * 
+ * <li><a class="lozenge"
+ * href="/secure/IssueNavigator.jspa?reset=true&amp;jqlQuery=labels+%3D+workshop"
+ * title="workshop"><span>workshop</span></a></li>
+ * 
+ * b) edit mode (example: field in the "Link issues dialog"):
+ * 
+ * <li class="item-row" title="ISSUE-50"> <button type="button" tabindex="-1"
+ * class="value-item"> <span><span class="value-text">ISSUE-50</span></span>
+ * </button> <em class="item-delete" title="Remove"></em> </li>
+ */
+
+var PROCESSED_FLAG = 'data-cpy-processed';
+
+function extendTagLikeItems() {
+  // readonly targets
+  var targets = $('li:has(> .lozenge)').filter(':not([' + PROCESSED_FLAG + '])');
+  
+  for (var i = 0; i < targets.length; i++) {
+    trace('Adding copy button to readonly item #' + (i + 1));
+    var target = targets[i];
+    addCopyButton(target);
+  }
+  
+  // editable targets
+  targets = $('.item-row:not([' + PROCESSED_FLAG + '])');
+  
+  for (var i = 0; i < targets.length; i++) {
+    trace('Adding copy button to editable item #' + (i + 1));
+    var target = targets[i];
+    addCopyButton(target);
+  }
+}
+
+function addCopyButton(target) {
+  $(target).attr(PROCESSED_FLAG, true);
+  
+  var copyBtn = getCopyButton();
+  copyBtn.visBinder.addTriggerEl(target);
+}
+
+function getCopyButton() {
+  if (typeof copyBtn != 'undefined') {
+    return copyBtn;
+  }
+  
+  copyBtn = $('<div class="item-copy" title="Copy text to clipboard"'
+      + ' style="z-index: 1000000; position: absolute; display: none; color: red; border: 1px solid red; background-color: white; opacity: 0.75;'
+      + ' width: 14px; height: 20px; font-size: 16px; line-height: 16px; text-align: center; font-style: italic; font-weight: bold; cursor: pointer;">'
+      + '<span style="vertical-align: middle">c</span></div>')[0];
+  insertBodyElement(copyBtn);
+  
+  copyBtn.visBinder = new VisibilityBinder(copyBtn, {
+    timeout : 2000
+  });
+  
+  $(copyBtn).on('click', function() {
+    $(this).hide();
+    copyToClipboard($(this.triggerEl).text());
+  });
+  
+  return copyBtn;
+}
+
+// @class (reusable)
+function VisibilityBinder(targetEl, cfg) {
+  this.targetEl = targetEl;
+  
+  var timeout = cfg.timeout;
+  var timeoutT;
+  
+  $(targetEl).on('mouseover', function() {
+    if (timeoutT) {
+      clearTimeout(timeoutT);
+      timeoutT = null;
+    }
+  });
+  $(targetEl).on('mouseout', function() {
+    timeoutT = setTimeout(function() {
+      $(targetEl).hide();
+    }, timeout);
+  });
+  
+  this.addTriggerEl = function(triggerEl) {
+    $(triggerEl).on('mouseover', function() {
+      $(targetEl).show();
+      // this could be configurable as well:
+      $(targetEl).position({ my: 'center top', at: 'center bottom', of: triggerEl });
+      
+      targetEl.triggerEl = triggerEl;
+  
+      if (timeoutT) {
+        clearTimeout(timeoutT);
+        timeoutT = null;
+      }
+    });
+  
+    $(triggerEl).on('mouseout', function() {
+      timeoutT = setTimeout(function() {
+        $(targetEl).hide();
+      }, timeout);
+    });
+  }
 }
